c8428da6996dee685c002a7aa03f1bbde01f27fa
pavana kartik <pavanakartikvelagaelti@gmail.com>
2020-06-01 01:49:04 +0530

    Implemented GetVehicle by Id Api

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -93,5 +93,17 @@ namespace kartzmax.Controllers {
 
         }
 
+        [HttpGet ("api/vehicles/{id}")]
+        public async Task<IActionResult> GetVehicle (int id) {
+            var vehicle = await context.Vehicles.Include (v => v.Features).SingleOrDefaultAsync (v => v.Id == id);
+
+            if (vehicle == null)
+                return NotFound ();
+
+            var vehicleResource = mapper.Map<Vehicle, VehicleResource> (vehicle);
+
+            return Ok (vehicleResource);
+        }
+
     }
 }
