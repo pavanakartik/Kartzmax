5414a71e907f42f5ab239ca1161d2bd2b659c7b5
pavana kartik <pavanakartikvelagaelti@gmail.com>
2020-06-04 02:20:57 +0530

    Implementing Cascading Dropdown lists

--- ClientApp/src/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/src/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,5 +1,6 @@
 import { MakeService } from './../../services/make.service';
 import { Component, OnInit } from '@angular/core';
+import { SelectControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-vehicle-form',
@@ -9,12 +10,19 @@ import { Component, OnInit } from '@angular/core';
 export class VehicleFormComponent implements OnInit {
 
   makes: any;
+  vehicle: any ={};
+  models: any;
 
-  constructor( private makeService: MakeService) { }
+  constructor(private makeService: MakeService) { }
 
   ngOnInit() {
 
-    this.makeService.getMakes().subscribe(makes=>{this.makes= makes});
+    this.makeService.getMakes().subscribe(makes => { this.makes = makes });
+  }
+
+  onMakeChange() {
+    var selectedMake = this.makes.find(m => m.id == this.vehicle.makeId);
+    this.models = selectedMake ? selectedMake.models : [];
   }
 
 }
