4598531117fec1992dc9bf41d874794b58506a08
pavana kartik <pavanakartikvelagaelti@gmail.com>
2020-06-08 00:54:52 +0530

    Implemented Client side filtering

--- ClientApp/src/app/components/vehicle-list/vehicle-list.component.ts
+++ ClientApp/src/app/components/vehicle-list/vehicle-list.component.ts
@@ -1,3 +1,4 @@
+import { KeyValuePair } from './../../models/vehicle';
 import { Component, OnInit } from '@angular/core';
 import { Vehicle, SaveVehicle } from 'src/app/models/vehicle';
 import { VehicleService } from 'src/app/services/vehicle.service';
@@ -11,13 +12,37 @@ export class VehicleListComponent implements OnInit {
 
 
   vehicles: any;
+  allVehicles: any;
+  makes: any;
+  filter: any = {};
 
 
   constructor(private vehicleService: VehicleService) { }
 
   ngOnInit() {
+    this.vehicleService.getMakes()
+      .subscribe(makes => this.makes = makes);
 
-    this.vehicleService.getVehicles().subscribe(vehicles => this.vehicles = vehicles);
+    this.vehicleService.getVehicles()
+      .subscribe(vehicles => this.vehicles = this.allVehicles = vehicles);
+
+  }
+
+  onFilterChange() {
+    var vehicles = this.allVehicles;
+
+    if (this.filter.makeId)
+      vehicles = vehicles.filter(v => v.make.id == this.filter.makeId);
+
+    if (this.filter.modelId)
+      vehicles = vehicles.filter(v => v.model.id == this.filter.modelId);
+
+    this.vehicles = vehicles;
+  }
+
+  resetFilter() {
+    this.filter = {};
+    this.onFilterChange();
   }
 
 }
